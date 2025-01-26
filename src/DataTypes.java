37b0b15c72a451023a03b06f3d9ebdbd5c45ea01
Gio <giorgisagrishvili86@gmail.com>
2025-01-26 10:54:28 +0400

    made homework project

---
+++ src/DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+
+        byte Byte = 100;
+        short Short = 30000;
+        int Int = 100000;
+        long Long = 10000000000L;
+        float Float = 3.14f;
+        double Double = 3.141;
+        char Char = 'a';
+        boolean Boolean = true;
+
+
+
+
+
+        String String = "hello world!";
+
+        System.out.println("Byte: " + Byte);
+        System.out.println("Short: " + Short);
+        System.out.println("Int: " + Int);
+        System.out.println("Long: " + Long);
+        System.out.println("Float: " + Float);
+        System.out.println("Double: " + Double);
+        System.out.println("Char: " + Char);
+        System.out.println("Boolean: " + Boolean);
+        System.out.println("String: " + String);
+    }
+}
